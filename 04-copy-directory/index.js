65462b2804364e468a2c6a581a7ab4508b0b10bb
Zhiznevski <w.heisenberggg@gmail.com>
2023-05-06 16:19:16 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require('path');
 
+const filesCopyFolder = path.join(__dirname, '/files-copy');
+const filesFolder = path.join(__dirname, '/files');
+
+fs.mkdir(filesCopyFolder, { recursive: true }, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+fs.readdir(filesFolder, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach(file => {
+      fs.createWriteStream(`${filesCopyFolder}/${file.name}`);
+    });
+  }
+});
+
+fs.readdir(filesFolder, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    console.log(err);
+  } else {
+    files.forEach(file => {
+      fs.copyFile(`${filesFolder}/${file.name}`, `${filesCopyFolder}/${file.name}`, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+});
