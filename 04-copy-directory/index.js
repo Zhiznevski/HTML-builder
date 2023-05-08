061f18d8f15f221b39b96b475b929407c74cd85c
Zhiznevski <w.heisenberggg@gmail.com>
2023-05-08 13:00:48 +0300

    refactor: add promises to 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,32 +4,47 @@ const path = require('path');
 const filesCopyFolder = path.join(__dirname, '/files-copy');
 const filesFolder = path.join(__dirname, '/files');
 
-fs.mkdir(filesCopyFolder, { recursive: true }, (err) => {
-  if (err) {
-    console.log(err);
-  }
+let promise = new Promise(function(resolve) {
+  fs.rm(filesCopyFolder, { recursive:true, force:true }, (err) => {
+    if(err){
+      console.error(err.message);
+    } else {
+      resolve();
+    }
+  });
+});
+promise.then(function resolve() {
+  getCopy();
 });
 
-fs.readdir(filesFolder, {withFileTypes: true}, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach(file => {
-      fs.createWriteStream(`${filesCopyFolder}/${file.name}`);
-    });
-  }
-});
+function getCopy() {
+  fs.mkdir(filesCopyFolder, { recursive: true }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 
-fs.readdir(filesFolder, {withFileTypes: true}, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach(file => {
-      fs.copyFile(`${filesFolder}/${file.name}`, `${filesCopyFolder}/${file.name}`, (err) => {
-        if (err) {
-          console.log(err);
-        }
+  fs.readdir(filesFolder, {withFileTypes: true}, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach(file => {
+        fs.createWriteStream(`${filesCopyFolder}/${file.name}`);
       });
-    });
-  }
-});
+    }
+  });
+
+  fs.readdir(filesFolder, {withFileTypes: true}, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach(file => {
+        fs.copyFile(`${filesFolder}/${file.name}`, `${filesCopyFolder}/${file.name}`, (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      });
+    }
+  });
+}
